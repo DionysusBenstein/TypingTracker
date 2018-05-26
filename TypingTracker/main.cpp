8eb1d8ff20dc2257a6b02441ea15095c59d679a8
DionysusBenstein <gromovdenis962@gmail.com>
2018-05-26 08:26:02 +0300

    Add implementation encryptLogs() function

--- TypingTracker/main.cpp
+++ TypingTracker/main.cpp
@@ -24,11 +24,126 @@
 //using namespace std;
 
 bool is_capslock = false;
-int backspace = 0; 
+int backspace = 0;    
+std::string high_en_alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+std::string low_en_alph = "abcdefghijklmnopqrstuvwxyz";
+std::string high_ru_alph = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+std::string low_ru_alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+std::string signs = "!\"#$%^&*()+=-_'?.,|/`~№:;@[]{}";
+std::string numbers = "0123456789";
 
-void encryptLogs()
+std::string encryptLogs(const std::string str, const int key)
 {
+	std::string output;
 
+//Посимвольное прохождение по введённой строке
+for (int i = 0; i < str.length(); ++i)
+{
+	//Если в введённой строке встречается пробел, то он остаётся неизменённым
+	if (str[i] == ' ')
+	{
+		output += ' ';
+	}
+
+	//Посимвольное прохождение по английскому алфавиту в верхнем регистре
+	for (int j = 0; j < high_en_alph.length(); ++j)
+	{
+		if (high_en_alph[j] == str[i]) //Нахождение совпадений в алфавите
+		{
+			if ((j + key) > 25) //Проверка на выход за границы массива
+			{
+				//Сдвиг индекса в начало алфавита
+				output += high_en_alph[(j - 26) + key];
+			}
+			else
+			{
+				//Сдвиг j-элемента на key позиций вправо
+				output += high_en_alph[j + key];
+			}
+		}
+	}
+
+	//Посимвольное прохождение по английскому алфавиту в нижнем регистре
+	for (int j = 0; j < low_en_alph.length(); ++j)
+	{
+		if (low_en_alph[j] == str[i])
+		{
+			if ((j + key) > 25)
+			{
+				output += low_en_alph[(j - 26) + key];
+			}
+			else
+			{
+				output += low_en_alph[j + key];
+			}
+		}
+	}
+
+	//Посимвольное прохождение по русскому алфавиту в верхнем регистре
+	for (int j = 0; j < high_ru_alph.length(); ++j)
+	{
+		if (high_ru_alph[j] == str[i])
+		{
+			if ((j + key) > 32)
+			{
+				output += high_ru_alph[(j - 33) + key];
+			}
+			else
+			{
+				output += high_ru_alph[j + key];
+			}
+		}
+	}
+
+	//Посимвольное прохождение по русскому алфавиту в нижнем регистре
+	for (int j = 0; j < low_ru_alph.length(); ++j)
+	{
+		if (low_ru_alph[j] == str[i])
+		{
+			if ((j + key) > 32)
+			{
+				output += low_ru_alph[(j - 33) + key];
+			}
+			else
+			{
+				output += low_ru_alph[j + key];
+			}
+		}
+	}
+
+	//Посимвольное прохождение по строчке со знаками
+	for (int j = 0; j < signs.length(); ++j)
+	{
+		if (signs[j] == str[i])
+		{
+			if ((j + key) > 29)
+			{
+				output += signs[(j - 30) + key];
+			}
+			else
+			{
+				output += signs[j + key];
+			}
+		}
+	}
+
+	//Посимвольное прохождение по строчке с цифрами
+	for (int j = 0; j < numbers.length(); ++j)
+	{
+		if (numbers[j] == str[i])
+		{
+			if ((j + key) > 9)
+			{
+				output += numbers[(j - 10) + key];
+			}
+			else
+			{
+				output += numbers[j + key];
+			}
+		}
+	}
+}
+return output;
 }
 
 int save(const int key)
